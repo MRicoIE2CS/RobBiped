89c9cce8931d7456f4e2d31a7b6c4891a2c5f5ee
MRicoIE2C <mrico.ie2@gmail.com>
2023-04-14 10:30:53 +0200

    Range specification for anaglog read and style corrections
    Signed-off-by: MRicoIE2C <mrico.ie2@gmail.com>

--- RobBiped/RobBiped/UserInput/UserInput.cpp
+++ RobBiped/RobBiped/UserInput/UserInput.cpp
@@ -24,16 +24,16 @@ void UserInput::assoc_GPIO(Configs::UserInputPins &_gpio){
 }
 
 void UserInput::configuration(){
-	
+
 	forward_button_.setup(gpio_->forward_button, INPUT_PULLDOWN);
 	back_button_.setup(gpio_->back_button, INPUT_PULLDOWN);
-	
+
 	potentiometer1_.setup(gpio_->potentiometer1, 0.97);
 	potentiometer2_.setup(gpio_->potentiometer2, 0.97);
 }
 
 void UserInput::update(){
-	
+
 	potentiometer1_.read_HW_value();
 	potentiometer2_.read_HW_value();
 	forward_button_.read_HW_value();
@@ -41,7 +41,7 @@ void UserInput::update(){
 }
 
 uint16_t UserInput::get_analog_value(AnalogInputList select_input){
-	
+
 	switch (select_input) {
 		case AnalogInputList::potentiometer1:
 			return potentiometer1_.get_value();
@@ -54,7 +54,7 @@ uint16_t UserInput::get_analog_value(AnalogInputList select_input){
 
 
 bool UserInput::get_digital_value(DigitalInputList select_input){
-	
+
 	switch (select_input) {
 		case DigitalInputList::forward_button:
 			return forward_button_.get_value();
