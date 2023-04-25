e6864c95c6b63e17eedf330f5320a50987ae64a0
MRicoIE2C <mrico.ie2@gmail.com>
2023-04-25 17:16:58 +0200

    Intermediate development of automatic force sensors tare operation. Order improvements in both files
    Signed-off-by: MRicoIE2C <mrico.ie2@gmail.com>

--- RobBiped/RobBiped/Main/Executor.h
+++ RobBiped/RobBiped/Main/Executor.h
@@ -21,16 +21,18 @@
 
 #include "Arduino.h"
 
+#include "Configs.h"
 #include "I_PeriodicTask.h"
 #include "../Actuators/JointsManager.h"
-#include "../Utils/SignalGenerator.h"
-#include "Configs.h"
-#include "../Utils/ExponentialFilter.h"
-#include "../UserInput/UserInput.h"
-#include "../Sensors/ForceSensorsManager.h"
-#include "../Sensors/GyroscopeAccelerometerManager.h"
-#include "../Control/TorsoPosture/TorsoPosture.h"
 #include "../Control/FootSupport/FootRollCentering.h"
+#include "../Control/TorsoPosture/TorsoPosture.h"
+#include "../Sensors/GyroscopeAccelerometerManager.h"
+#include "../Sensors/ForceSensorsManager.h"
+#include "../UserInput/UserInput.h"
+#include "../Utils/Control/TrayectoryInterpolator.h"
+#include "../Utils/Control/Waiting.h"
+#include "../Utils/ExponentialFilter.h"
+#include "../Utils/SignalGenerator.h"
 
 class Executor {
 
@@ -49,38 +51,93 @@ class Executor {
 		Control::TorsoPosture torso_posture_controller_;
 		Control::FootRollCentering left_foot_roll_centering_controller_;
 		Control::FootRollCentering right_foot_roll_centering_controller_;
-
-		SignalGenerator squats_unitary_cycle_generator_;
 		///// END OBJECT TASKS __//
 
-		/////____________ AUXILIARY OBJECTS: __//
-		uint8_t state_number = 0;
+		/////____________ APPLICATION EXCLUSIVE OBJECTS AND METHODS: __//
 
+		// State machine related objects and methods
+		uint8_t state_number = 0;
+		void state_machine_switch();
+		void state_machine_execution();
+		void read_commands();
+		void always_executes();
+		bool state0_first_time = true;
+		bool state0_finished = false;
+		void state0_execution();
+		uint8_t state1_phase = 0;
+		bool state1_finished = false;
+		void state1_execution();
+		bool state2_first_time = true;
+		bool state2_finished = false;
+		void state2_execution();
+		bool state3_first_time = true;
+		bool state3_finished = false;
+		void state3_execution();
+		bool state4_first_time = true;
+		bool state4_finished = false;
+		void state4_execution();
+		bool state5_first_time = true;
+		bool state5_finished = false;
+		void state5_execution();
+		bool state6_first_time = true;
+		bool state6_finished = false;
+		void state6_execution();
+
+		// Other state machine flags
+		bool automatic_force_tare_on = false;
+
+		// STATE 1 Trajectory generation objects
+		bool state1_trajectory_running_ = false;
+		double state1_angle_displacement_ = 1.0;
+		double state1_legs_squat_displacement = 30.0;
+		double state1_leg_lift_displacement = 10.0;
+		const uint64_t state1_trajectory_time_ = 3000;
+		const double home_position_leg_length_ = 142.0;
+		Control::LinearTrajectoryInterpolator state1_interpolator1_;
+		double last_hip_angle_compensation = 0.0;
+		
+		// Control tasks' objects
+		double torso_upright_pitch_control_action = 0.0;
 		double torso_setpoint_ = 0.0;
 		ExpFilter torso_pitch_exp_filter_;
 		double zmp_lateral_deviation_setpoint_ = 0.0;
 		ExpFilter left_zmp_lateral_exp_filter_;
 		ExpFilter right_zmp_lateral_exp_filter_;
 
-		bool squats_on = false;
-		bool squats_first_time = true;
-		///// END AUXILIARY OBJECTS: __//
+		SignalGenerator squats_unitary_cycle_generator_;
+		Control::LinearTrajectoryInterpolator trajectory_interpolator_;
+		bool interpolator_running_ = false;
+		const double min_desired_leg_length_ = 95.0;
+		const double max_desired_leg_length_ = 142.0;
+		double desired_leg_lenght_ = 0.0;
+		const uint64_t trajectory_time_ms_ = 10000;
+
+		// Enabling flag for squats movement
+		bool squats_on_ = false;
+		// Flag that is true on the first execution of each single linear trajectory of the squats
+		bool squats_first_time_ = true;
+		// Flag that indicates the direction of the movement on each iteration
+		bool squats_going_down_ = true;
+		// Flag that propagates an over-limit joint angle error on the current execution
+		bool squats_overlimit_error_propagation_ = false;
+
+		// Waiting object
+		Control::Waiting waiting_;
+		// Waiting first time
+		bool waiting_first_time_ = true;
+		// Waiting time between linear squat movements
+		uint64_t waiting_time_ = 2000;
+
+		///// END APPLICATION EXCLUSIVE OBJECTS AND METHODS: __//
 
 		/////____________ PRIVATE METHODS: __//
 		void associations();
 		void initialize_servo_setpoints();
+		void initialize_application();
 
 		void inputs();
 		void main_execution();
 		void outputs();
-
-		// State machine related methods
-		void state_machine_switch();
-		void state_machine_execution();
-		void read_commands();
-		void state0_execution();
-		void state1_execution();
-		void state2_execution();
 		///// END PRIVATE METHODS: __//
 
 	public:
--- RobBiped/RobBiped/Main/StateMachine.cpp
+++ RobBiped/RobBiped/Main/StateMachine.cpp
@@ -12,10 +12,35 @@
 
 #include "../Utils/Kinematics/ForwardKinematics.h"
 #include "../Utils/Kinematics/GeometricInverseKinematics.h"
+#include "../Utils/Kinematics/InverseKinematicsBlocks/LegLength_IK.h"
 #include "../Utils/LinearAlgebra/ArduinoEigenDense.h"
 
 using Eigen::IOFormat;
 using Eigen::Matrix2d;
+
+void Executor::read_commands()
+{
+	// Squats commands
+	if (command_->commands.squats_on)
+	{
+		squats_on_ = true;
+		Serial.println("squats_on::: ");
+		command_->commands.squats_on = false;
+	}
+	if (command_->commands.squats_off)
+	{
+		squats_on_ = false;
+		Serial.println("squats_off::: ");
+		command_->commands.squats_off = false;
+	}
+	
+	// Automatic tare commands
+	if (command_->commands.force_auto_tare)
+	{
+		command_->commands.force_auto_tare = false;
+		automatic_force_tare_on = true;
+	}
+}
 
 void Executor::state_machine_switch()
 {
@@ -24,26 +49,42 @@ void Executor::state_machine_switch()
 	switch (state_number)
 	{
 		case 0:
-			if (squats_on)
+			if (automatic_force_tare_on)
+			{
+				state_number++;
+				state1_phase = 0;
+				break;
+			}
+			break;
+		case 1:
+			if (state1_phase && state1_finished)
+			{
+				state_number++;
+				waiting_first_time_ = true;
+				break;
+			}
+			break;
+		case 2:
+			if (!waiting_first_time_ && state2_finished)
 			{
 				state_number++;
 				break;
 			}
 			break;
-		case 1:
-			if (!squats_on)
+		case 3:
+			if (state3_finished)
 			{
-				state_number--;
+				state_number = 4;
 				break;
 			}
-			break;
-		case 2:
 			break;
 	}
 }
 
 void Executor::state_machine_execution()
 {
+	always_executes();
+
 	switch (state_number)
 	{
 		case 0:
@@ -55,147 +96,38 @@ void Executor::state_machine_execution()
 		case 2:
 			state2_execution();
 			break;
+		case 3:
+			state3_execution();
+			break;
+		case 4:
+			state4_execution();
+			break;
+		case 5:
+			state5_execution();
+			break;
+		case 6:
+			state6_execution();
+			break;
 	}
 }
 
-void Executor::read_commands()
+void Executor::always_executes()
 {
-	// Squats commands
-	if (command_->commands.squats_on)
-	{
-		squats_on = true;
-		Serial.println("squats_on::: ");
-		command_->commands.squats_on = false;
-	}
-	if (command_->commands.squats_off)
-	{
-		squats_on = false;
-		Serial.println("squats_off::: ");
-		command_->commands.squats_off = false;
-	}
-	//__________________
-}
-
-void Executor::state0_execution()
-{
-	squats_first_time = true;
-}
-
-void Executor::state1_execution()
-{
-	// If sensors have been updated, control can be computed.
 	if (gyroscope_accelerometer_manager_.has_been_updated)
 	{
-		// Squats generation:::
-		// With the signal generator, direct kinematics are applied to form a squat.
-		double sine_unitary_signal = 0.0;
-		double potentiometer_value = 0.0;
-		if (squats_on)
-		{
-			if (squats_first_time)
-			{
-				Serial.println("squats_first time::: ");
-				squats_unitary_cycle_generator_.init();
-				squats_first_time = false;
-			}
-
-			//sine_unitary_signal = squats_unitary_cycle_generator_.generate_trajectory();
-			potentiometer_value = user_input_.get_analog_value(UserInput::AnalogInputList::potentiometer1) / 4095.0;
-
-			if (command_->commands.squats_debug_on)
-			{
-				Serial.println("Squats signal::: " + (String)potentiometer_value);
-
-				if (command_->commands.squats_debug_off)
-				{
-					command_->commands.squats_debug_on = false;
-					command_->commands.squats_debug_off = false;
-				}
-			}
-
-		}
-
-		IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
-
-		// Desired leg length
-		double desired_leg_lenght = 75 + (142-75) * potentiometer_value;
-		// Lengths definition
-		const double thigh = 79;
-		const double calf = 63;
-
-		// INVERSE KINEMATICS
-
-		double forward_angle_rad = 0.0;
-		double lateral_angle_rad = 0.0;	// Not used
-		Vector3d desired_position;
-		Serial.println("Initial leg position: ");
-		Serial.println("desired_leg_lenght: " + (String)desired_leg_lenght);
-		Serial.println("forward_angle_rad: " + (String)forward_angle_rad);
-		Serial.println("lateral_angle_rad: " + (String)lateral_angle_rad);
-
-		ForwardKinematics::Geometric::get_position_from_length_and_angles(desired_leg_lenght, forward_angle_rad, lateral_angle_rad, desired_position);
-		Serial.println("Desired position: ");
-		std::cout << desired_position.format(CleanFmt) << std::endl;
-
-		Vector2d links_lengths;
-		links_lengths << calf, thigh;
-		double target_angle_rad_1;
-		double target_angle_rad_2;
-		bool ret_val = InverseKinematics::Geometric::sagittal_two_links_inverse_kinematics(desired_position, links_lengths, target_angle_rad_1, target_angle_rad_2);
-		Serial.println("target joint angles: " + (String)(target_angle_rad_1 * RAD_TO_DEG) + ", " + (String)(target_angle_rad_2 * RAD_TO_DEG));
-
-		// OBTAIN TRTANSFORMATION MATRIX to know the orientation of final the effector
-
-		// Denavit-Hartenberg table
-		std::vector<Vector4d> DH_table;
-		Vector4d DH_row_1;
-		DH_row_1 << target_angle_rad_1, 0, calf, 0;
-		Vector4d DH_row_2;
-		DH_row_2 << target_angle_rad_2, 0, thigh, 0;
-		DH_table.push_back(DH_row_1);
-		DH_table.push_back(DH_row_2);
-
-		Matrix4d TM;
-
-		if (ret_val) ForwardKinematics::DenavitHartenberg::get_overall_TM_from_DH_table(DH_table, TM);
-
-		Serial.println("TF: ");
-		std::cout << TM.format(CleanFmt) << std::endl;
-
-		Matrix2d rotation_matrix = TM.block<2,2>(0,0);
-		Serial.println("rotation_matrix: ");
-		std::cout << rotation_matrix.format(CleanFmt) << std::endl;
-
-		double angle_to_compensate_hip = 0.0;
-		if (ret_val & squats_on)
-		{
-			angle_to_compensate_hip = atan2(TM(0,1), TM(0,0));
-
-			// Servo setpoint assignation.
-			servo_updater_.set_angle_to_servo(Configuration::JointsNames::LeftFootPitch, target_angle_rad_1);
-			servo_updater_.set_angle_to_servo(Configuration::JointsNames::LeftKnee, target_angle_rad_2);
-			servo_updater_.set_angle_to_servo(Configuration::JointsNames::RightFootPitch, target_angle_rad_1);
-			servo_updater_.set_angle_to_servo(Configuration::JointsNames::RightKnee, target_angle_rad_2);
-			servo_updater_.should_be_updated = true;
-		}
-
-		Serial.println("target joint angles: " + (String)(angle_to_compensate_hip * RAD_TO_DEG));
-
 		// Torso upright control:::
+
 		// Get torso pitch orientation measurement.
 		double filtered_torso_pitch_angle_rad = torso_pitch_exp_filter_.filter(gyroscope_accelerometer_manager_.get_value_angle_z_pitch_rad());
-
 		// Compute the control action for torso pitch.
-		double torso_pitch_control_action = torso_posture_controller_.compute(filtered_torso_pitch_angle_rad);
-
-		// Servo setpoint assignation.
-		servo_updater_.set_angle_to_servo(Configuration::JointsNames::LeftHipPitch, -torso_pitch_control_action + angle_to_compensate_hip);
-		servo_updater_.set_angle_to_servo(Configuration::JointsNames::RightHipPitch, -torso_pitch_control_action + angle_to_compensate_hip);
-		servo_updater_.should_be_updated = true;
+		torso_upright_pitch_control_action = torso_posture_controller_.compute(filtered_torso_pitch_angle_rad);
 	}
 
 	if (force_sensors_manager_.has_been_updated)
 	{
+		// Feet roll control:::
+		// TODO: Correct the deviation of the joint axis respect to the foot axis
+
 		double left_foot_roll_centering_action = 0.0;
 		if (force_sensors_manager_.is_tare_left_performed())
 		{
@@ -217,13 +149,288 @@ void Executor::state1_execution()
 		}
 
 		// Servo setpoint assignation.
-		servo_updater_.set_angle_to_servo(Configuration::JointsNames::LeftFootRoll, left_foot_roll_centering_action);
-		servo_updater_.set_angle_to_servo(Configuration::JointsNames::RightFootRoll, right_foot_roll_centering_action);
+		servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftFootRoll, left_foot_roll_centering_action);
+		servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightFootRoll, right_foot_roll_centering_action);
 		servo_updater_.should_be_updated = true;
+	}
+}
+
+void Executor::state0_execution()
+{
+	if (!state0_first_time) state0_first_time = true;
+
+	// Robot starts in home position: All Joints' angles = 0.
+	// After "servo.pwr" command, the "force.tar.auto" command starts the execution of the automatic tare of the force sensors
+	// located on the feet. After typing the starting command, "torso.on" command could be sent for that controller to better
+	// stabilize the torso posture.
+}
+
+void Executor::state1_execution()
+{
+	// STATE 1: Pivot to the right foot.
+	// In this state, from home position, the robot pivots to the right in order to stand on the right foot.
+	// The movement is divided in three different linear movements.
+	// First, a squat movement, then the pivot, and after that, a small retraction of the left leg to leave more space with the floor.
+
+	bool trajectory1_running = false;
+	if (0 == state1_phase)
+	{
+		if (!state1_interpolator1_.configure_trayectory(1.0, 0.0, state1_trajectory_time_))
+		while (true)
+		{
+			Serial.println("ERR_BAD_PARAMETER");
+			delay(1000);
+		}
+		state1_phase = 1;
+	}
+	if (1 == state1_phase)
+	{
+		if (state1_interpolator1_.get_execution_flag())
+		{
+			// TRAJECTORY UNITARY COMPUTATION
+			double trajectory_unitary_vector;
+			trajectory1_running = state1_interpolator1_.compute_output(trajectory_unitary_vector);
+			
+			// LEG LIFT DISPLACEMENT
+			double calculated_leg_length = home_position_leg_length_ - state1_legs_squat_displacement * trajectory_unitary_vector;
+
+			//Inverse kinematics
+			double forward_angle_rad = 0.0;
+			double ankle_target_angle;
+			double knee_target_angle;
+			double hip_angle_compensation;
+
+			bool ret_val_0 = InverseKinematics::get_leg_joints_angles_from_desired_length_and_orientation(
+			calculated_leg_length, forward_angle_rad,
+			config_.kinematics.height_knee_ankle, config_.kinematics.height_hip_knee,
+			ankle_target_angle, knee_target_angle, hip_angle_compensation);
+
+			// Joint setpoint assignation.
+			bool ret_val1 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftFootPitch, ankle_target_angle);
+			bool ret_val2 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftKnee, knee_target_angle);
+			bool ret_val5 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftHipPitch, -torso_upright_pitch_control_action + hip_angle_compensation);
+			bool ret_val3 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightFootPitch, ankle_target_angle);
+			bool ret_val4 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightKnee, knee_target_angle);
+			bool ret_val6 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightHipPitch, -torso_upright_pitch_control_action + hip_angle_compensation);
+			last_hip_angle_compensation = hip_angle_compensation;
+
+			servo_updater_.should_be_updated = true;
+			
+			if (!trajectory1_running)
+			{
+				state1_phase = 2;
+				state1_interpolator1_.reset();
+			}
+		}
+	}
+	if (2 == state1_phase)
+	{
+		if (state1_interpolator1_.get_execution_flag())
+		{
+			// TRAJECTORY UNITARY COMPUTATION
+			double trajectory_unitary_vector;
+			trajectory1_running = state1_interpolator1_.compute_output(trajectory_unitary_vector);
+
+			// LATERAL DISPLACEMENT
+			double lateral_displacement_angle = state1_angle_displacement_ * trajectory_unitary_vector;
+
+			servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftFootRoll, lateral_displacement_angle);
+			servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftHipRoll, lateral_displacement_angle/6.0);
+			servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightFootRoll, -lateral_displacement_angle);
+
+			// This allows the torso upright controller to still actuate over the hip pitch joints
+			bool ret_val5 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftHipPitch, -torso_upright_pitch_control_action + last_hip_angle_compensation);
+			bool ret_val6 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightHipPitch, -torso_upright_pitch_control_action + last_hip_angle_compensation);
+
+			servo_updater_.should_be_updated = true;
+
+			if (!trajectory1_running)
+			{
+				state1_phase = 3;
+				state1_interpolator1_.reset();
+			}
+		}
+	}
+	if (3 == state1_phase)
+	{
+		if (state1_interpolator1_.get_execution_flag())
+		{
+			// TRAJECTORY UNITARY COMPUTATION
+			double trajectory_unitary_vector;
+			trajectory1_running = state1_interpolator1_.compute_output(trajectory_unitary_vector);
+			
+			// LEG LIFT DISPLACEMENT
+			double calculated_leg_length = home_position_leg_length_ - state1_legs_squat_displacement - state1_leg_lift_displacement * trajectory_unitary_vector;
+
+			//Inverse kinematics
+			double forward_angle_rad = 0.0;
+			double ankle_target_angle;
+			double knee_target_angle;
+			double hip_angle_compensation;
+
+			bool ret_val_0 = InverseKinematics::get_leg_joints_angles_from_desired_length_and_orientation(
+			calculated_leg_length, forward_angle_rad,
+			config_.kinematics.height_knee_ankle, config_.kinematics.height_hip_knee,
+			ankle_target_angle, knee_target_angle, hip_angle_compensation);
+
+			// Joint setpoint assignation.
+			bool ret_val1 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftFootPitch, ankle_target_angle);
+			bool ret_val2 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftKnee, knee_target_angle);
+			bool ret_val5 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftHipPitch, -torso_upright_pitch_control_action + hip_angle_compensation);
+			last_hip_angle_compensation = hip_angle_compensation;
+
+			servo_updater_.should_be_updated = true;
+
+			if (!trajectory1_running)
+			{
+				state1_finished = true;
+				state1_interpolator1_.reset();
+			}
+		}
 	}
 }
 
 void Executor::state2_execution()
 {
-	
+	// STATE 2: Waiting.
+	// The robot waits a few seconds in order to gather data for the tare of the left foot.
+
+	if (waiting_first_time_)
+	{
+		waiting_first_time_ = false;
+		trajectory_interpolator_.reset();
+		waiting_.configure_waiting(waiting_time_);
+		if (squats_going_down_) squats_going_down_ = false;
+		else squats_going_down_ = true;
+	}
+	if (waiting_.evaluate())
+	{
+		// Set command to tare left foot.
+		command_->commands.force_tare_left = true;
+		state2_finished = true;
+	}
+}
+
+void Executor::state3_execution()
+{
+}
+
+void Executor::state4_execution()
+{
+}
+
+void Executor::state5_execution()
+{
+}
+
+void Executor::state6_execution()
+{
+	// If sensors have been updated, control can be computed.
+	if (gyroscope_accelerometer_manager_.has_been_updated)
+	{
+		// Squats generation:::
+		// Using the trajectory obtained from an interpolator, direct kinematics are applied to form a squat.
+
+		if (squats_first_time_)
+		{
+			double target_leg_length;
+			double current_leg_length;
+			if (squats_going_down_)
+			{
+				target_leg_length = min_desired_leg_length_;
+				current_leg_length = max_desired_leg_length_;
+			}
+			else
+			{
+				target_leg_length = max_desired_leg_length_;
+				current_leg_length = min_desired_leg_length_;
+			}
+			if (!trajectory_interpolator_.configure_trayectory(target_leg_length, current_leg_length, trajectory_time_ms_))
+				while (true)
+				{
+					Serial.println("ERR_BAD_PARAMETER");
+					delay(1000);
+				}
+			squats_first_time_ = false;
+		}
+
+		// Desired leg length computation (linear interpolation)
+		interpolator_running_ = trajectory_interpolator_.compute_output(desired_leg_lenght_);
+
+		// Debug prints
+		if (command_->commands.squats_debug_on)
+		{
+			Serial.println("desired_leg_lenght_::: " + (String)desired_leg_lenght_);
+
+			if (command_->commands.squats_debug_off)
+			{
+				command_->commands.squats_debug_on = false;
+				command_->commands.squats_debug_off = false;
+			}
+		}
+
+		// INVERSE KINEMATICS
+
+		double forward_angle_rad = 0.0;
+		double lateral_angle_rad = 0.0;	// Not used
+		Vector3d desired_position;
+		ForwardKinematics::Geometric::get_position_from_length_and_angles(desired_leg_lenght_, forward_angle_rad, lateral_angle_rad, desired_position);
+
+		Vector2d links_lengths;
+		links_lengths << config_.kinematics.height_knee_ankle, config_.kinematics.height_hip_knee;
+		double target_angle_rad_1;
+		double target_angle_rad_2;
+		bool ret_val = InverseKinematics::Geometric::sagittal_two_links_inverse_kinematics(desired_position, links_lengths, target_angle_rad_1, target_angle_rad_2);
+
+		// DIRECT KINEMATICS: OBTAIN TRTANSFORMATION MATRIX to know the orientation of final the effector
+
+		// Denavit-Hartenberg table
+		std::vector<Vector4d> DH_table;
+		Vector4d DH_row_1;
+		DH_row_1 << target_angle_rad_1, 0, config_.kinematics.height_knee_ankle, 0;
+		Vector4d DH_row_2;
+		DH_row_2 << target_angle_rad_2, 0, config_.kinematics.height_hip_knee, 0;
+		DH_table.push_back(DH_row_1);
+		DH_table.push_back(DH_row_2);
+
+		Matrix4d TM;
+
+		if (ret_val) ForwardKinematics::DenavitHartenberg::get_overall_TM_from_DH_table(DH_table, TM);
+
+		Matrix2d rotation_matrix = TM.block<2,2>(0,0);
+
+		double angle_to_compensate_hip = 0.0;
+		if (ret_val)
+		{
+			angle_to_compensate_hip = atan2(TM(0,1), TM(0,0));
+
+			// Servo setpoint assignation.
+			squats_overlimit_error_propagation_ = false;
+			bool ret_val1 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftFootPitch, target_angle_rad_1);
+			bool ret_val2 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftKnee, target_angle_rad_2);
+			bool ret_val3 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightFootPitch, target_angle_rad_1);
+			bool ret_val4 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightKnee, target_angle_rad_2);
+			if (!ret_val1 || !ret_val2 || !ret_val3 || !ret_val4)
+			{
+				// In the case that any of the joints has reached an over-limit angle, the current action is not applied
+				servo_updater_.revert_angle_to_joint(Configuration::JointsNames::LeftFootPitch);
+				servo_updater_.revert_angle_to_joint(Configuration::JointsNames::LeftKnee);
+				servo_updater_.revert_angle_to_joint(Configuration::JointsNames::RightFootPitch);
+				servo_updater_.revert_angle_to_joint(Configuration::JointsNames::RightKnee);
+				squats_overlimit_error_propagation_ = true;
+			}
+			else servo_updater_.should_be_updated = true;
+		}
+
+		// Servo setpoint assignation.
+		bool ret_val5 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::LeftHipPitch, -torso_upright_pitch_control_action + angle_to_compensate_hip);
+		bool ret_val6 = servo_updater_.set_angle_to_joint(Configuration::JointsNames::RightHipPitch, -torso_upright_pitch_control_action + angle_to_compensate_hip);
+		if (squats_overlimit_error_propagation_ || !ret_val5 || !ret_val6)
+		{
+			// In the case that any of the joints has reached an over-limit angle, the current action is not applied
+			servo_updater_.revert_angle_to_joint(Configuration::JointsNames::LeftHipPitch);
+			servo_updater_.revert_angle_to_joint(Configuration::JointsNames::RightHipPitch);
+		}
+		else servo_updater_.should_be_updated = true;
+	}
 }
