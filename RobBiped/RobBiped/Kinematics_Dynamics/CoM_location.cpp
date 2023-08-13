b5255903e72e47d371028248b5ee52d710d855c4
MRicoIE2C <mrico.ie2@gmail.com>
2023-08-13 19:10:17 +0200

    Double integration and LIPM methods fusion by complement filter
    Signed-off-by: MRicoIE2C <mrico.ie2@gmail.com>

--- RobBiped/RobBiped/Kinematics_Dynamics/CoM_location.cpp
+++ RobBiped/RobBiped/Kinematics_Dynamics/CoM_location.cpp
@@ -18,30 +18,101 @@
 
 #include "CoM_location.h"
 
-void CoMLocation::set_CoM_height(const double &_CoM_height)
+void CoMLocation::init_location(Vector3d &_initial_location)
 {
-	CoM_location_(2) = _CoM_height;
+	last_CoM_location_ = _initial_location;
+	last_millis_ = millis();
 }
 
-Vector3d CoMLocation::compute_position_from_LIPM(const Vector3d &_CoM_acceleration_measurements_ms2_xyz, const Vector2d &_CoM_inclination_xy, const Vector2d &_ZMP_position_xy)
+void CoMLocation::set_CoM_height(const double &_CoM_height)
 {
-	CoM_inclination_xy_ = _CoM_inclination_xy;
+	last_CoM_location_(2) = _CoM_height;
+}
 
-	Vector3d corrected_CoM_acceleration_xyz;
-	CoM_acceleration_ = corrected_CoM_acceleration_xyz;
+void CoMLocation::set_filter_complement_k(const double &_kf)
+{
+	Kf = _kf;
+}
 
-	// From LIP Model:
-	double h = CoM_location_(2);
-	CoM_location_(0) = h / gravity_constant_ * (_CoM_acceleration_measurements_ms2_xyz(0) *1000.0) + _ZMP_position_xy(0);
-	CoM_location_(1) = h / gravity_constant_ * (_CoM_acceleration_measurements_ms2_xyz(1) *1000.0) + _ZMP_position_xy(1);
+Vector2d CoMLocation::compute_location_from_LIPM(Vector3d &_acc_measure_mms2_xyz, Vector2d &_ZMP_position_xy)
+{
+	Vector2d com_location;
+	double h = last_CoM_location_(2);
+	com_location(0) = h / gravity_constant_ * _acc_measure_mms2_xyz(0) + _ZMP_position_xy(0);
+	com_location(1) = h / gravity_constant_ * _acc_measure_mms2_xyz(1) + _ZMP_position_xy(1);
+	
+	return com_location;
+}
+
+Vector2d CoMLocation::compute_location_from_integration(Vector3d &_acc_mean_mms2_xyz, double &_time_incr)
+{
+	Vector2d final_velocity;
+	final_velocity(0) = last_CoM_velocity_(0) + _acc_mean_mms2_xyz(0) * _time_incr;
+	final_velocity(1) = last_CoM_velocity_(1) + _acc_mean_mms2_xyz(1) * _time_incr;
+
+	Vector2d mean_velocity;
+	mean_velocity(0) = last_CoM_velocity_(0) + (final_velocity(0) - last_CoM_velocity_(0)) / 2.0;
+	mean_velocity(1) = last_CoM_velocity_(1) + (final_velocity(1) - last_CoM_velocity_(1)) / 2.0;
+
+	// Position incrementation
+	Vector2d com_location;
+	com_location(0) = last_CoM_location_(0) + mean_velocity(0) * _time_incr;
+	com_location(1) = last_CoM_location_(1) + mean_velocity(1) * _time_incr;
+
+	return com_location;
+}
+
+Vector3d CoMLocation::compute_location(Vector3d &_CoM_acceleration_measurements_ms2_xyz, Vector2d &_ZMP_position_xy)
+{
+	// Adequate inputs
+	uint32_t current_millis = millis();
+	double time_incr = static_cast<double>(current_millis - last_millis_)/1000.0;	// (s)
+	Vector3d new_acc_measure_mms2_xyz;
+	new_acc_measure_mms2_xyz(0) = _CoM_acceleration_measurements_ms2_xyz(0)*1000.0;
+	new_acc_measure_mms2_xyz(1) = _CoM_acceleration_measurements_ms2_xyz(1)*1000.0;
+	new_acc_measure_mms2_xyz(2) = _CoM_acceleration_measurements_ms2_xyz(2)*1000.0;
+	Vector3d mean_acc_mms2;
+	mean_acc_mms2(0) = last_CoM_acceleration_(0) + (new_acc_measure_mms2_xyz(0) - last_CoM_acceleration_(0)) / 2.0;
+	mean_acc_mms2(1) = last_CoM_acceleration_(1) + (new_acc_measure_mms2_xyz(1) - last_CoM_acceleration_(1)) / 2.0;
+	mean_acc_mms2(2) = last_CoM_acceleration_(2) + (new_acc_measure_mms2_xyz(2) - last_CoM_acceleration_(2)) / 2.0;
+
+	// Computation from each method
+	Vector2d intgr_com_location = compute_location_from_integration(mean_acc_mms2, time_incr);
+	Vector2d lipm_com_location = compute_location_from_LIPM(new_acc_measure_mms2_xyz, _ZMP_position_xy);
+
+	// Fusion
+	Vector3d fused_com_location;
+	fused_com_location(0) = Kf * intgr_com_location(0,0) + (1.0-Kf) * lipm_com_location(0);
+	fused_com_location(1) = Kf * intgr_com_location(1,0) + (1.0-Kf) * lipm_com_location(1);
+
+	// Update memory values
+	last_CoM_acceleration_(0) = new_acc_measure_mms2_xyz(0);
+	last_CoM_acceleration_(1) = new_acc_measure_mms2_xyz(1);
+	last_CoM_velocity_(0) = (last_CoM_location_(0) - fused_com_location(0)) / time_incr;
+	last_CoM_velocity_(1) = (last_CoM_location_(1) - fused_com_location(1)) / time_incr;
+	last_CoM_location_(0) = fused_com_location(0);
+	last_CoM_location_(1) = fused_com_location(1);
+	last_millis_ = current_millis;
 	
 // 	Serial.println("__DEBUG___");
-// 	Serial.println("_acc_measure_xyz: \t" + (String)_CoM_acceleration_measurements_xyz(0) + "\t" + (String)_CoM_acceleration_measurements_xyz(1) + "\t" + (String)_CoM_acceleration_measurements_xyz(2));
-// 	Serial.println("_corrected_acc_xyz: \t" + (String)corrected_CoM_acceleration_xyz(0) + "\t" + (String)corrected_CoM_acceleration_xyz(1) + "\t" + (String)corrected_CoM_acceleration_xyz(2));
-// 	Serial.println("CoM_incl_xy: \t" + (String)_CoM_inclination_xy(0) + "\t" + (String)_CoM_inclination_xy(1));
-// 	Serial.println("ZMP_xy: \t" + (String)_ZMP_position_xy(0) + "\t" + (String)_ZMP_position_xy(1));
-// 	
-// 	Serial.println("CoM_xy: \t" + (String)CoM_location_(0) + "\t" + (String)CoM_location_(1));
+ 	Serial.println("a, itgr, lipm, fusd, zmp: \t" + (String)new_acc_measure_mms2_xyz(1) + "\t" + (String)intgr_com_location(1) + "\t" + (String)lipm_com_location(1) + "\t" + (String)fused_com_location(1) + "\t" + (String)_ZMP_position_xy(1));
+
+// 	//Serial.println("_acc_measure_xyz: \t" + (String)_CoM_acceleration_measurements_xyz(0) + "\t" + (String)_CoM_acceleration_measurements_xyz(1) + "\t" + (String)_CoM_acceleration_measurements_xyz(2));
+// 	//Serial.println("_corrected_acc_xyz: \t" + (String)corrected_CoM_acceleration_xyz(0) + "\t" + (String)corrected_CoM_acceleration_xyz(1) + "\t" + (String)corrected_CoM_acceleration_xyz(2));
+// 	Serial.println("Kf: \t" + (String)Kf);
+// 	Serial.println("acc_measure_mms2_xyz: \t" + (String)acc_measure_mms2_xyz(1));
+// 	Serial.println("intgr_com_location: \t" + (String)intgr_com_location(1));
+// 	Serial.println("lipm_com_location: \t" + (String)lipm_com_location(1));
+// 	Serial.println("fused_com_location: \t" + (String)fused_com_location(1));
+// 	Serial.println("_ZMP_position_xy: \t" + (String)_ZMP_position_xy(1));
+// 
+// 	Serial.print("time_incr: \t");
+// 	Serial.println(time_incr, 5);
 	
-	return CoM_location_;
+	return last_CoM_location_;
+}
+
+Vector3d CoMLocation::get_location()
+{
+	return last_CoM_location_;
 }
--- RobBiped/RobBiped/Kinematics_Dynamics/CoM_location.h
+++ RobBiped/RobBiped/Kinematics_Dynamics/CoM_location.h
@@ -32,22 +32,45 @@ class CoMLocation {
 
 	private:
 
-		Vector3d CoM_location_;
-		Vector3d CoM_velocity_;
-		Vector3d CoM_acceleration_;
-		Vector2d CoM_inclination_xy_;
+		// Estimated variables (mm | mm/s | mm/s^2)
+		Vector3d last_CoM_location_;
+		Vector3d last_CoM_velocity_;
+		Vector3d last_CoM_acceleration_;
 
-		// Gravity constant, in mm/s^2
+		// Constant of the complement filter for the estimation of the CM location [0.0-1.0]
+		double Kf = 1;
+
+		// Gravity constant (mm/s^2)
 		const double gravity_constant_ = 9807;
 
+		// Milliseconds when the location was last executed
+		uint32_t last_millis_ = millis();
+
+		// Compute position from Linear Inverted Pendulum Model
+		Vector2d compute_location_from_LIPM(Vector3d &_acc_measure_mms2_xyz, Vector2d &_ZMP_position_xy);
+
+		// Compute position integrating from accelerometer measurement
+		Vector2d compute_location_from_integration(Vector3d &_acc_mean_mms2_xyz, double &_time_incr);
+
 	public:
+
+		// Initiate position
+		void init_location(Vector3d &_initial_location);
 
 		// Set CM height.
 		// CoM is considered restricted to an horizontal plane, in LIPM
 		void set_CoM_height(const double &_CM_height);
 
-		// Compute position from Linear Inverted Pendulum Model
-		Vector3d compute_position_from_LIPM(const Vector3d &_CoM_acceleration_measurements_ms2_xyz, const Vector2d &_CoM_inclination_xy, const Vector2d &_ZMP_position_xy);
+		// Set the constant of the complement filter for the estimation of the CM location [0.0-1.0]
+		// A value of 1.0, gives all the weight to the calculation from the integration of the acceleration measurements
+		// A value of 0.0, gives all the weight to the calculation from the linear inverted pendulum model and the acceleration and ZMP measurements
+		void set_filter_complement_k(const double &_kf);
+
+		// Compute position
+		Vector3d compute_location(Vector3d &_CoM_acceleration_measurements_ms2_xyz, Vector2d &_ZMP_position_xy);
+
+		// Compute position
+		Vector3d get_location();
 };
 
 #endif
