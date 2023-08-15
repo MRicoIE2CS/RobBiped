730f2c32906ce5ab0c9eef7f1fa5cadc90293c19
MRicoIE2C <mrico.ie2@gmail.com>
2023-08-15 13:55:19 +0200

    Clean
    Signed-off-by: MRicoIE2C <mrico.ie2@gmail.com>

--- RobBiped/RobBiped/Kinematics_Dynamics/CoM_location.cpp
+++ RobBiped/RobBiped/Kinematics_Dynamics/CoM_location.cpp
@@ -18,9 +18,15 @@
 
 #include "CoM_location.h"
 
+#include "cmath"
+
 void CoMLocation::init_location(Vector3d &_initial_location)
 {
 	last_CoM_location_ = _initial_location;
+	last_CoM_velocity_(0) = 0.0;
+	last_CoM_velocity_(1) = 0.0;
+	last_CoM_acceleration_(0) = 0.0;
+	last_CoM_acceleration_(1) = 0.0;
 	last_millis_ = millis();
 }
 
@@ -41,14 +47,13 @@ Vector2d CoMLocation::compute_location_from_LIPM(Vector3d &_acc_measure_mms2_xyz
 	double h = last_CoM_location_(2);
 	com_location(0) = h / gravity_constant_ * _acc_measure_mms2_xyz(0) + _ZMP_position_xy(0);
 	com_location(1) = h / gravity_constant_ * _acc_measure_mms2_xyz(1) + _ZMP_position_xy(1);
-	
+
 	return com_location;
 }
 
 Matrix2d CoMLocation::compute_location_from_integration(Vector3d &_acc_mean_mms2_xyz, double &_time_incr)
 {
 	Matrix2d final_location_velocity;
-	//Vector2d final_velocity;
 	final_location_velocity(0,1) = last_CoM_velocity_(0) + _acc_mean_mms2_xyz(0) * _time_incr;
 	final_location_velocity(1,1) = last_CoM_velocity_(1) + _acc_mean_mms2_xyz(1) * _time_incr;
 
@@ -57,7 +62,6 @@ Matrix2d CoMLocation::compute_location_from_integration(Vector3d &_acc_mean_mms2
 	mean_velocity(1) = (final_location_velocity(1,1) + last_CoM_velocity_(1)) / 2.0;
 
 	// Position incrementation
-	//Vector2d com_location;
 	final_location_velocity(0,0) = last_CoM_location_(0) + mean_velocity(0) * _time_incr;
 	final_location_velocity(1,0) = last_CoM_location_(1) + mean_velocity(1) * _time_incr;
 
@@ -101,21 +105,10 @@ Vector3d CoMLocation::compute_location(Vector3d &_CoM_acceleration_measurements_
 	last_CoM_location_(0) = fused_com_location(0);
 	last_CoM_location_(1) = fused_com_location(1);
 	last_millis_ = current_millis;
-	
-// 	Serial.println("__DEBUG___");
- 	//Serial.println("a, itgr, lipm, fusd, zmp: \t" + (String)new_acc_measure_mms2_xyz(1) + "\t" + (String)intgr_com_location_velocity(1,0) + "\t" + (String)lipm_com_location(1) + "\t" + (String)fused_com_location(1) + "\t" + (String)_ZMP_position_xy(1));
-	//Serial.println("a, itgr, lipm, fusd, zmp: \t" + (String)new_acc_measure_mms2_xyz(0) + "\t" + (String)intgr_com_location(0) + "\t" + (String)lipm_com_location(0) + "\t" + (String)fused_com_location(0) + "\t" + (String)_ZMP_position_xy(0));
 
- 	//Serial.println("_corrected_acc_xyz: \t" + (String)new_acc_measure_mms2_xyz(0) + "\t" + (String)new_acc_measure_mms2_xyz(1) + "\t" + (String)new_acc_measure_mms2_xyz(2));
-// 	Serial.println("Kf: \t" + (String)Kf);
-// 	Serial.println("acc_measure_mms2_xyz: \t" + (String)acc_measure_mms2_xyz(1));
-// 	Serial.println("intgr_com_location: \t" + (String)intgr_com_location(1));
-// 	Serial.println("lipm_com_location: \t" + (String)lipm_com_location(1));
-// 	Serial.println("fused_com_location: \t" + (String)fused_com_location(1));
-// 	Serial.println("_ZMP_position_xy: \t" + (String)_ZMP_position_xy(1));
-// 
-// 	Serial.print("time_incr: \t");
-// 	Serial.println(time_incr, 5);
+// 	Serial.println("__DEBUG___");
+//Serial.println("a, itgr, lipm, fusd, zmp: \t" + (String)new_acc_measure_mms2_xyz(1) + "\t" + (String)intgr_com_location_velocity(1,0) + "\t" + (String)lipm_com_location(1) + "\t" + (String)fused_com_location(1) + "\t" + (String)_ZMP_position_xy(1));
+//Serial.println("a, itgr, lipm, fusd, zmp: \t" + (String)new_acc_measure_mms2_xyz(0) + "\t" + (String)intgr_com_location(0) + "\t" + (String)lipm_com_location(0) + "\t" + (String)fused_com_location(0) + "\t" + (String)_ZMP_position_xy(0));
 	
 	return last_CoM_location_;
 }
