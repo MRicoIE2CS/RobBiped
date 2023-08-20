3be5046d7ac19c239b39c657e76038b2026eb4aa
MRicoIE2C <mrico.ie2@gmail.com>
2023-08-20 19:38:55 +0200

    New command for new test
    Signed-off-by: MRicoIE2C <mrico.ie2@gmail.com>

--- RobBiped/RobBiped/UserInput/Command.cpp
+++ RobBiped/RobBiped/UserInput/Command.cpp
@@ -36,15 +36,23 @@ void Command::listen_for_commands()
 		if (command.equals("init")){
 			commands.init = true;
 		}
+	// SERVO POWER ENABLE
 		else if (command.equals("servo.pwr")){
 			commands.servo_onoff_toggle = true;
 		}
+	// SERVO CALIBRATION
 		else if (command.equals("servo.calib")){
 			commands.servo_calibration_onoff_toggle = true;
 		}
 		else if (command.equals("servo.sel")){
 			commands.servo_selection_button_emulation = true;
 		}
+	// ENABLE NOTIFICATIONS FOR OVERLIMIT ANGLE
+		else if (command.equals("show.servo.limits")){
+			if (commands.show_alarm_angle_limit) commands.show_alarm_angle_limit = false;
+			else commands.show_alarm_angle_limit = true;
+		}
+	// FORCE SENSORS MANAGEMENT
 		else if (command.equals("force.tar.left")){
 			commands.force_tare_left = true;
 		}
@@ -70,6 +78,7 @@ void Command::listen_for_commands()
 		else if (command.equals("zmp.deb.off")){
 			commands.zmp_debug_off = true;
 		}
+	// GYROSCOPE/ACCELEROMETER MANAGEMENT
 		else if (command.equals("gyro.cal.on")){
 			commands.gyroacc_calibrate_on = true;
 		}
@@ -82,6 +91,7 @@ void Command::listen_for_commands()
 		else if (command.equals("gyro.deb.off")){
 			commands.gyroacc_debug_off = true;
 		}
+	// TORSO POSTURE CONTROLLER
 		else if (command.equals("torso.deb.on")){
 			commands.torso_posture_debug_on = true;
 		}
@@ -94,6 +104,7 @@ void Command::listen_for_commands()
 		else if (command.equals("torso.off")){
 			commands.torso_posture_off = true;
 		}
+	// ZMP TRACKING CONTROLLER
 		else if (command.equals("zmptr.deb")){
 			if (commands.zmp_tracking_deb_toggle) commands.zmp_tracking_deb_toggle = false;
 			else commands.zmp_tracking_deb_toggle = true;
@@ -106,21 +117,25 @@ void Command::listen_for_commands()
 			if (commands.zmp_ytracking_toggle) commands.zmp_ytracking_toggle = false;
 			else commands.zmp_ytracking_toggle = true;
 		}
+	// APPLICATION TOGGLE
 		else if (command.equals("app.tgl")){
 			if (commands.application_toggle) commands.application_toggle = false;
 			else commands.application_toggle = true;
 		}
+	// GET UP AND DOWN ROUTINE
 		else if (command.equals("get.up")){
 			if (commands.get_up_toggle) commands.get_up_toggle = false;
 			else commands.get_up_toggle = true;
 		}
+	// X-BALANCE TEST, (state number 10)
 		else if (command.equals("xbalance")){
 			if (commands.test_x_balance_toggle) commands.test_x_balance_toggle = false;
-			else commands.test_x_balance_toggle = true;
+				else commands.test_x_balance_toggle = true;
 		}
-		else if (command.equals("show.servo.limits")){
-			if (commands.show_alarm_angle_limit) commands.show_alarm_angle_limit = false;
-			else commands.show_alarm_angle_limit = true;
+	// Y OFFLINE TRAJECTORY TRACKING TEST, (state number 20)
+		else if (command.equals("ytrack")){
+			if (commands.test_y_offline_tracking_toggle) commands.test_y_offline_tracking_toggle = false;
+			else commands.test_y_offline_tracking_toggle = true;
 		}
 		else if (command.equals("off"))
 		{
